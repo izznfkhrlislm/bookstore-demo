2b6372302a311c72223f20f1a87d299df1b0562f
Izzan Fakhril Islam <izzanfi@hotmail.com>
2025-09-08 22:35:40 +0700

    fixing env testing issue

--- src/test/java/id/izzanfi/bookstoreserviceapi/BookstoreServiceApiApplicationTests.java
+++ src/test/java/id/izzanfi/bookstoreserviceapi/BookstoreServiceApiApplicationTests.java
@@ -2,8 +2,10 @@ package id.izzanfi.bookstoreserviceapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("DEV")
 class BookstoreServiceApiApplicationTests {
 
   @Test
